7ee8592a9f183215e2c8d7f88b92aa69d03c06da
karim <karim.dev.hamza@gmail.com>
2019-05-17 12:46:57 +0000

    first commit

---
+++ src/main/java/com/miraki/projectpmtool/ProjectpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.miraki.projectpmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectpmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectpmtoolApplication.class, args);
+    }
+
+}
